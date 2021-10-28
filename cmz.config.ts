7d2a20849bc4aef4f85742c750cd438877f1cc42
Harrison Lau <harrison.lau@outlook.com>
2021-10-28 18:14:45 +0800

    refactor: add cmz and editorconfig

---
+++ cmz.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "@shad02w/cmz";
+
+export default defineConfig({
+    commitTypes: [
+        {
+            name: "exercise",
+            description: "complete new exercises",
+        },
+        {
+            name: "review",
+            description: "review exercises",
+        },
+        {
+            name: "refactor",
+            description: "refactor exercises",
+        },
+        {
+            name: "fix",
+            description: "bug fix",
+        },
+        {
+            name: "upgrade",
+            description: "dependencies update and change",
+        },
+    ],
+    // Final commit message reolve to
+    resolve: ({ commitType, message }) => `${commitType.name}: ${message}`,
+});
